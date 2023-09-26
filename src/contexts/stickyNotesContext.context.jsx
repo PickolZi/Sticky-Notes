b485e7c9cce084fcaf04ac13295d316a75ff1f15
PickolZi <pickol876@gmail.com>
2023-09-26 10:53:10 -0700

    User session is now saved in local storage. When page refreshes/user comes back to site, they will remained logged in.

--- src/contexts/stickyNotesContext.context.jsx
+++ src/contexts/stickyNotesContext.context.jsx
@@ -34,7 +34,6 @@ export const StickyNotesProvider = ({children}) => {
         }))
   
     }
-
     const value = {
         stickyNotesJSON, 
         setStickyNotesJSON, 
--- src/contexts/userContext.context.jsx
+++ src/contexts/userContext.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 // Create context to use anywhere within components
 export const userContext = createContext(
@@ -8,10 +8,42 @@ export const userContext = createContext(
     }
 );
 
+
+const setUserSession = (userCredentialsContext) => {
+    // Saves user's session into local storage.
+    if (userCredentialsContext) {
+        console.log("Setting local user auth: ")
+        console.log(JSON.stringify(userCredentialsContext))
+        localStorage.setItem('user-auth', JSON.stringify(userCredentialsContext))
+    } else {
+        console.log("Removing user auth: ")
+        localStorage.removeItem('user-auth')
+    }
+}
+
+const getUserSession = (setUserCredentialsContext) => {
+    // If old user session is still valid. Keep user logged in.
+    let userLocalSession = localStorage.getItem('user-auth')
+    console.log("Got user session: ", userLocalSession)
+    if (userLocalSession) {
+        setUserCredentialsContext(JSON.parse(userLocalSession));
+    }
+}
+
 // Creating provider necessary for context.
 export const UserProvider = ({children}) => {
     const [userCredentialsContext, setUserCredentialsContext] = useState(null);
     const value = {userCredentialsContext, setUserCredentialsContext};
+
+    useEffect(() => {
+        getUserSession(setUserCredentialsContext);
+    }, [])
+
+    useEffect(() => {
+        // console.log("Something happened to the user.")
+        setUserSession(userCredentialsContext);
+    }, [userCredentialsContext]);
+    
     
     return (
         <userContext.Provider value={value}>{children}</userContext.Provider>
